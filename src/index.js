f01ff2208aef0ce16b4a814f5daea47e3d05d7b7
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-21 23:05:12 +0100

    Update linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom"
 //component file
 import TodoContainer from  './functionBased/components/TodoContainer'
 import './functionBased/App.css'
-import { BrowserRouter } from "react-router-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
